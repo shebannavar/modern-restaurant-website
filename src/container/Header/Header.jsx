4785e919c6759484ed183a54c36a6428c4f7cd51
shebannavar <santosh.shebannavar@gmail.com>
2022-04-08 20:01:20 +0530

    initial commit

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {SubHeading} from '../../components';
+import './Header.css';
+import {images} from '../../constants';
+
+const Header = () => (
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+    <SubHeading title="Chase the new flavour"/>
+    <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+    <p className='p__opensans' style={{margin:'2rem 0'}}>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, eos illum debitis vel quo quibusdam maiores eius dignissimos corrupti est dolores id possimus molestiae recusandae tempore architecto vero saepe necessitatibus.
+    </p>
+    <button type='button' className='custom__button'>Explore Menu</button>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt="header img" />
+    </div>
+  </div>
+);
+
+export default Header;
